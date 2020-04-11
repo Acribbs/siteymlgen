bc96e65b45ca8e5f7f025f65337559131b372a75
Acribbs <adam.cribbs@imm.ox.ac.uk>
2020-04-11 20:49:58 +0100

    modified init function to remove siteymlgen name

--- R/init_siteymlgen.R
+++ R/init_siteymlgen.R
@@ -38,7 +38,7 @@
 #' @importFrom magrittr %>%
 #' @return A _site.yml file
 #' @export
-init_siteymlgen <- function(authors=NULL, date=lubridate::today(),
+init <- function(authors=NULL, date=lubridate::today(),
                             affiliation=NULL, dir=".", left=NULL, right=NULL,
                             navbar_title=NULL, title="siteymlgen", categories=NULL,
                             name=NULL, output_dir=NULL, include=NULL,
